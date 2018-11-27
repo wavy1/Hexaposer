70d8aaa036f887de3772818c120db191f22636ef
Klaus <crazy_crawler@gmx.net>
2018-11-27 10:38:25 +0100

    cleanUp

--- hexaposer_server/main.js
+++ hexaposer_server/main.js
@@ -6,7 +6,6 @@ app.use(express.static('static'));
 //Serves all the request which includes /images in the url from Images folder
 app.use('/images', express.static(__dirname + '/static/images'));
 app.use('/fonts', express.static(__dirname + '/static/fonts'));
-app.use('/flip_test', express.static(__dirname + '/static/flip_test'));
 app.use('/css', express.static(__dirname + '/static/css'));
 app.use('/js', express.static(__dirname + '/static/js'));
 
