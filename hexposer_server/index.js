07b72a7a419fcb930efedab4d7e59306379acae9
Klaus <crazy_crawler@gmx.net>
2018-12-14 11:39:58 +0100

    whatever

--- hexposer_server/index.js
+++ hexposer_server/index.js
@@ -5,6 +5,12 @@ var fs = require('fs');
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
 
+app.use(express.static('static'));
+app.use('/images', express.static(__dirname + '/static/images'));
+app.use('/fonts', express.static(__dirname + '/static/fonts'));
+app.use('/css', express.static(__dirname + '/static/css'));
+app.use('/js', express.static(__dirname + '/static/js'));
+
 // open json and read to buffer
 let myjson = fs.readFileSync('info.json');
 // parsing the json data
@@ -15,6 +21,7 @@ var hexagons = [];
 for (var i = 0; i < test.Hexagons.length; i++){
   hexagons[i] = test.Hexagons[i];
 }
+console.log(hexagons.length);
 // next passing hexagons into pug
 
 app.get('/dynamic_view', function(req, res){
@@ -22,7 +29,8 @@ app.get('/dynamic_view', function(req, res){
       
       //Hier müssten die .json Daten einfließen
       name: "TutorialsPoint", 
-      url:"http://www.tutorialspoint.com"
+      url:"http://www.tutorialspoint.com",
+      hexArr: hexagons
    });
 });
 
