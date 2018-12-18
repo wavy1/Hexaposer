11c6aa84be8ac6b49f626fc1e5171dac40daed4a
wavy1s <w.nabok@gmail.com>
2018-12-18 08:22:10 +0100

    Wrote Json parse as function and Color selection

--- hexposer_server/index.js
+++ hexposer_server/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var exec = require('child_process').execFile;
 var colorConv = require('./static/js/colorDefine.js');
 
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
+
+console.log("Start ImageProcessor");
+exec('../hexposer_image_processor/cmake-build-debug/hexaposer.exe', function(err, data) {
+   console.log(err);
+   console.log(data.toString());
+})
 
 app.use(express.static('static'));
 
@@ -15,15 +22,16 @@ app.use('/css', express.static(__dirname + '/static/css'));
 app.use('/js', express.static(__dirname + '/static/js'));
 
 
-// open json and read to buffer
-let myjson = fs.readFileSync('info.json');
-// parsing the json data
-var test = JSON.parse(myjson);
+var getHexagons = function() {
+  // open json and read to buffer
+  let myjson = fs.readFileSync('info.json');
+      // parsing the json data
+      var test = JSON.parse(myjson);
 // creating the hexgons array
 var hexagons = [];
 // iterating the content from parsed json to the array
 for (var i = 0; i < test.Hexagons.length; i++){
-  hexagons[i] = test.Hexagons[i];
+ hexagons[i] = test.Hexagons[i];
 }
 
 // console.log(hexagons);
@@ -31,30 +39,31 @@ for (var i = 0; i < test.Hexagons.length; i++){
 
 /*Color correction for the .json data */
 for(var i = 0; i < hexagons.length; i++){
-hexagons[i].color = colorConv.colorPrepare(hexagons[i].color);
-if (hexagons[i].color == 'ff0000'){
-   hexagons[i].color = 'Red';
+   hexagons[i].color = colorConv.colorPrepare(hexagons[i].color);
+   if (hexagons[i].color == 'ff0000'){
+      hexagons[i].color = 'Red';
    }
-else if(hexagons[i].color == 'ff9900'){
-   hexagons[i].color = 'Orange';
+   else if(hexagons[i].color == 'ff9900'){
+      hexagons[i].color = 'Orange';
    }
-else if(hexagons[i].color == '00ff00'){
-   hexagons[i].color = 'Green';
+   else if(hexagons[i].color == '00ff00'){
+      hexagons[i].color = 'Green';
    }
-else if(hexagons[i].color == '0000ff'){
+   else if(hexagons[i].color == '0000ff'){
       hexagons[i].color = 'Blue';
    }
-else if(hexagons[i].color == '990099'){
+   else if(hexagons[i].color == '990099'){
       hexagons[i].color = 'Purple';
    }
-else if(hexagons[i].color == '999999'){
+   else if(hexagons[i].color == '999999'){
       hexagons[i].color = 'Grey';
    }
-else if(hexagons[i].color == '00ffff'){
+   else if(hexagons[i].color == '00ffff'){
       hexagons[i].color = 'Turquoise';
    }
 }
- 
+return hexagons;
+
 /* Array of colors for later use
 
    var colors =
@@ -66,12 +75,14 @@ else if(hexagons[i].color == '00ffff'){
    {grey : '999999'},
    {cyan : '00ffff'}];
    
-*/
+   */
 
+
+}
 
 app.get('/dynamic_view', function(req, res){
    res.render('dynamic', {
-      hexArr : hexagons
+      hexArr : getHexagons()
       //Übergabe des .json Arrays an die dynamic_view
    });
 });
